1fef9fbc6c25acdc09b4736b4842d1ea7f55a87d
Rafael Sahb <sahb@tutanota.com>
2024-09-12 22:56:44 -0300

    Updated indents for PHP

--- after/queries/php/indents.scm
+++ after/queries/php/indents.scm
@@ -6,5 +6,6 @@
     (object_creation_expression))
   ; prevent double indent for `return function() { ... }`
   (return_statement
-    (anonymous_function_creation_expression))
+    ; changed as recommended by https://github.com/nvim-treesitter/nvim-treesitter-textobjects/pull/673
+    (anonymous_function))
 ] @indent.dedent
